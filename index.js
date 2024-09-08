675359302792e213deb571cab1a3ea552e704d4f
markglasman <glasman.mark@gmail.com>
2024-09-08 09:51:09 -0600

    fixed draw/clear cycles to keep player on screen at all times

--- index.js
+++ index.js
@@ -47,7 +47,6 @@ const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, "blue");
-player.draw();
 
 const projectile = new Projectile(
   canvas.width / 2,
@@ -69,6 +68,7 @@ const projectiles = [];
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height)
+  player.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
   });
