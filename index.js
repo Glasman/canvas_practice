d4ea6d030bd12b358c70dc6c2ad0d789fa7735bb
markglasman <glasman.mark@gmail.com>
2024-09-08 13:41:36 -0600

    fiddled around with enemy sizes

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ const enemies = [];
 
 function spawnEnemies() {
   setInterval(() => {
-    const radius = Math.random() * (26) + 7;
+    const radius = Math.random() * (26) + 10;
 
     let x
     let y
