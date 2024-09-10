933cbdf7efe14c1c52657e751056125ae27a3f28
markglasman <glasman.mark@gmail.com>
2024-09-10 17:14:23 -0400

    cleaned up animtion on enemy deletion

--- index.js
+++ index.js
@@ -129,9 +129,14 @@ function animate() {
     projectiles.forEach((projectile, projectileIndex) => {
       const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
 
+      //objects touch
       if (dist - enemy.radius - projectile.radius < 1) {
-        enemies.splice(index, 1);
-        projectiles.splice(index, 1);
+        //gets rid of occasional flash on removal of enemy
+        setTimeout(() => {
+          enemies.splice(index, 1);
+          projectiles.splice(index, 1);
+        }, 0)
+    
       }
     });
   });
